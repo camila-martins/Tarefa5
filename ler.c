77eeb04e1d37a9aad92924f3cd7cbcca6327922d
Camila Martins <37345577+camila-martins@users.noreply.github.com>
2018-05-13 19:42:24 -0300

    Add files via upload

---
+++ ler.c
@@ -0,0 +1,34 @@
+#include<linalg.h>
+
+FILE *fp;
+
+
+//Função para ler a matriz do arquivo 
+double **ler( char *matriz, int *L, int *C)
+{	
+	int  i, j, l, c;
+	double **M;
+	float a;
+	fp = fopen(matriz,"r");
+	i = fscanf(fp,"%d, %d", L, C);
+	
+	M = malloc( *L *sizeof(double *));
+	for(i=0; i<*L; i++)
+	{
+		M[i]= malloc((*C)*sizeof(double));
+	}	
+	i =0;
+	j = 0;
+
+	while (fscanf(fp,"%f",&a)!= EOF) 
+	{
+		M[i][j]=a;
+		j++;
+		if (j== *C) 
+		{	
+			j= 0;
+			i++;
+		}
+	}
+	return(M);
+}
